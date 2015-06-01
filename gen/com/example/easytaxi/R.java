4f182a605bb013baee19ec3ae9fedc17dbadc188
Viviana Fernandez <viviana.f8a@gmail.com>
2015-06-01 16:01:16 -0400

    actualicion de Android

--- gen/com/example/easytaxi/R.java
+++ gen/com/example/easytaxi/R.java
@@ -23,6 +23,7 @@ public final class R {
     public static final class drawable {
         public static final int easy=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int splash_screen=0x7f020002;
     }
     public static final class id {
         public static final int action_settings=0x7f080022;
@@ -68,6 +69,7 @@ public final class R {
         public static final int mapas=0x7f030003;
         public static final int registro=0x7f030004;
         public static final int registro2=0x7f030005;
+        public static final int splash_screen=0x7f030006;
     }
     public static final class menu {
         public static final int main=0x7f070000;
