d1545d45d0a8f82ad6f58d3f015a6de17ed82ffe
Viviana Fernandez <viviana.f8a@gmail.com>
2015-06-02 18:32:57 -0400

    act login

--- gen/com/example/easytaxi/R.java
+++ gen/com/example/easytaxi/R.java
@@ -23,20 +23,21 @@ public final class R {
     public static final class drawable {
         public static final int easy=0x7f020000;
         public static final int ic_launcher=0x7f020001;
-        public static final int splash_screen=0x7f020002;
+        public static final int rounded_edittext=0x7f020002;
+        public static final int splash_screen=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080022;
+        public static final int action_settings=0x7f080021;
         public static final int btnLogin=0x7f080012;
         public static final int btngps=0x7f080000;
         public static final int button1=0x7f080009;
         public static final int button2=0x7f08000b;
         public static final int button3=0x7f08000c;
-        public static final int celular=0x7f080019;
+        public static final int celular=0x7f080016;
         public static final int correo=0x7f08001a;
-        public static final int direccion=0x7f080017;
-        public static final int editText1=0x7f08001f;
-        public static final int editText4=0x7f08001e;
+        public static final int direccion=0x7f080018;
+        public static final int editText1=0x7f08001e;
+        public static final int editText4=0x7f08001d;
         public static final int imageView1=0x7f080004;
         public static final int linearLayout1=0x7f080001;
         public static final int linearLayout2=0x7f080003;
@@ -46,20 +47,19 @@ public final class R {
         public static final int linearLayout6=0x7f080011;
         public static final int link_registrer=0x7f080013;
         public static final int login_error=0x7f080014;
-        public static final int next=0x7f08001c;
-        public static final int nombre=0x7f080016;
+        public static final int next=0x7f08001b;
+        public static final int nombre=0x7f080017;
         public static final int password=0x7f080010;
-        public static final int personaContacto=0x7f08001b;
-        public static final int radioButton1=0x7f080021;
-        public static final int radioButton2=0x7f080020;
+        public static final int radioButton1=0x7f080020;
+        public static final int radioButton2=0x7f08001f;
         public static final int t=0x7f080015;
-        public static final int telDomicilio=0x7f080018;
+        public static final int telDomicilio=0x7f080019;
         public static final int textView1=0x7f080002;
         public static final int textView2=0x7f080006;
         public static final int textView3=0x7f080007;
         public static final int textView4=0x7f080008;
         public static final int tituloLogin=0x7f08000d;
-        public static final int tituloReg=0x7f08001d;
+        public static final int tituloReg=0x7f08001c;
         public static final int usuario=0x7f08000e;
     }
     public static final class layout {
